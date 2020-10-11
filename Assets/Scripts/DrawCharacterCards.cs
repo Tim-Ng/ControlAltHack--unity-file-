516c448a0891dcde12e049c63a8718c5e30183ef
Ng Yao Hong <efyyn2@nottingham.edu.my>
2020-10-11 13:01:16 +0000

    No Comments

---
+++ Assets/Scripts/DrawCharacterCards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Principal;
+using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+    public GameObject PlayerArea1;
+    public GameObject OponentArea1;
+    public GameObject OponentArea2;
+    public GameObject OponentArea3;
+    public GameObject OponentArea4;
+    public GameObject OponentArea5;
+    public GameObject CharCard1;
+
+    void Start()
+    {
+
+    }
+    public void OnClick()
+    {
+        GameObject characterCard = Instantiate(CharCard1, new Vector3(0,0,0),Quaternion.identity);
+    }
+}

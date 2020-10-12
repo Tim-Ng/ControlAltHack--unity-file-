5bd160035a6316d9a4636312a93c98a75ec776b6
Ng Yao Hong <efyyn2@nottingham.edu.my>
2020-10-12 16:31:51 +0000

    character template done

---
+++ Assets/Scripts/CharCardScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New CArd",menuName ="CharacterCard")]
+public class CharCardScript : ScriptableObject
+{
+    public string character_card_name;
+    public string discription;
+
+    public Sprite artwork_back;
+    public Sprite artwork_front;
+
+    public int input_hardware;
+    public int input_cryptanalysis;
+    public int input_network;
+    public int input_conections;
+    public int input_socal;
+    public int input_kitchen;
+    public int input_software;
+    public int input_special;
+
+    public string ad_disadvantages;
+
+    public string special;
+    public Sprite image_special;
+
+
+}
---
+++ Assets/Scripts/CharacterCardDispaly.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterCardDispaly : MonoBehaviour
+{
+    public CharCardScript CharCard;
+
+    public Text input_hardware;
+    public Text input_cryptanalysis;
+    public Text input_network;
+    public Text input_conections;
+    public Text input_socal;
+    public Text input_kitchen;
+    public Text input_software;
+    public Text input_special;
+    public Text discription;
+
+    public Text character_card_name;
+    public Text ad_disadvantages;
+    public Text special;
+
+
+    public Image image_special;
+    public Image artwork_back;
+    public Image artwork_front;
+    
+    
+    void Start()
+    {
+        character_card_name.text = CharCard.character_card_name;
+        ad_disadvantages.text = CharCard.ad_disadvantages;
+        special.text = CharCard.special;
+
+        input_hardware.text = CharCard.input_hardware.ToString();
+        input_cryptanalysis.text = CharCard.input_cryptanalysis.ToString();
+        input_network.text = CharCard.input_network.ToString();
+        input_conections.text = CharCard.input_conections.ToString();
+        input_socal.text = CharCard.input_socal.ToString();
+        input_kitchen.text = CharCard.input_kitchen.ToString();
+        input_software.text = CharCard.input_software.ToString();
+        input_special.text = CharCard.input_special.ToString();
+
+        artwork_back.sprite = CharCard.artwork_back;
+        artwork_front.sprite = CharCard.artwork_front;
+
+    }
+
+}

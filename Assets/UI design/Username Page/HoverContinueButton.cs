1ca3b75ecbd77deb543427a94df46a8ac5ada4ee
GohXinYee <sfyxg1@nottingham.edu.my>
2020-10-24 16:12:23 +0000

    okay initial design on the interface, not sure OKay or Not :'D

---
+++ Assets/UI design/Username Page/HoverContinueButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverContinueButton : MonoBehaviour
+{
+    //public GameObject continuebtn;
+    public Sprite unhoverimage;
+    public Sprite hoverimage;
+    public Image image;
+
+    public void OnHoverEnter()
+    {
+        Image image = GameObject.Find("Continue").GetComponent<Image>();
+        image.sprite = hoverimage;
+    }
+
+    public void OnHoverExit()
+    {
+        Image image = GameObject.Find("Continue").GetComponent<Image>();
+        image.sprite = unhoverimage;
+    }
+}

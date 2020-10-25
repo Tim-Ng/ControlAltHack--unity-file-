81f752797bf0caecdbce71cdebba25f938c158e3
GohXinYee <sfyxg1@nottingham.edu.my>
2020-10-25 13:15:39 +0000

    Added: Entropy Cards Added vars to EntropyCardScript

--- Assets/Scripts/MainGameScripts/EntropyCardScript.cs
+++ Assets/Scripts/MainGameScripts/EntropyCardScript.cs
@@ -8,6 +8,8 @@ public class EntropyCardScript : ScriptableObject
     public int EntropyCardID;
     public bool IsBagOfTricks;
     public bool IsLigthingStrikes;
+    public bool IsSharedFate;//added by goh
+    public bool IsExtensiveExperience;//added by goh
     public string Title;
     public string discription;
     
@@ -21,6 +23,7 @@ public class EntropyCardScript : ScriptableObject
     public bool use_usage;
     public bool UseSucFailLighting;
     public string Success;
+    public int add_how_much_cred;//added by goh
     public string Failure;
     public int minus_how_much_cred;
     public string RollVSWhich;

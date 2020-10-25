07bea0497f0fad2329f57b5648640b0ef799bc33
GohXinYee <sfyxg1@nottingham.edu.my>
2020-10-25 06:54:30 +0000

    touch-up touch-up

--- Assets/UI design/Username Page/HoverHostJoin.cs
+++ Assets/UI design/Username Page/HoverHostJoin.cs
@@ -8,17 +8,21 @@ public class HoverHostJoin : MonoBehaviour
     public Sprite unhoverimage;
     public Sprite hoverimage;
     public GameObject btn;
+    Vector3 unhoverscale;
 
     public void OnHoverEnter()
     {
         Image image = btn.GetComponent<Image>();
         image.sprite = hoverimage;
+        unhoverscale = btn.transform.localScale;
+        btn.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
     }
 
     public void OnHoverExit()
     {
         Image image = btn.GetComponent<Image>();
         image.sprite = unhoverimage;
+        btn.transform.localScale = unhoverscale;
     }
 
 }

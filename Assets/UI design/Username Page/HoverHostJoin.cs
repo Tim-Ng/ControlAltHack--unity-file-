5087a0597e52c4d3c0d67ec72fca3634a10a0cbc
GohXinYee <sfyxg1@nottingham.edu.my>
2020-10-25 06:43:32 +0000

    No Comments

---
+++ Assets/UI design/Username Page/HoverHostJoin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverHostJoin : MonoBehaviour
+{
+    public Sprite unhoverimage;
+    public Sprite hoverimage;
+    public GameObject btn;
+
+    public void OnHoverEnter()
+    {
+        Image image = btn.GetComponent<Image>();
+        image.sprite = hoverimage;
+    }
+
+    public void OnHoverExit()
+    {
+        Image image = btn.GetComponent<Image>();
+        image.sprite = unhoverimage;
+    }
+
+}

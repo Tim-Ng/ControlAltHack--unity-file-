09997240e731f097f3316289cdf8342430c85083
Ng Yao Hong <efyyn2@nottingham.edu.my>
2020-10-25 15:06:36 +0000

    add entropy to cards

--- Assets/Scripts/MainGameScripts/EntropyCardDeck.cs
+++ Assets/Scripts/MainGameScripts/EntropyCardDeck.cs
@@ -4,32 +4,85 @@ using UnityEngine;
 
 public class EntropyCardDeck : MonoBehaviour
 {
-    public EntropyCardScript entropyCardScript1;
-    public EntropyCardScript entropyCardScript2;
-    public EntropyCardScript entropyCardScript3;
-    public EntropyCardScript entropyCardScript4;
-    public EntropyCardScript entropyCardScript5;
-    public EntropyCardScript entropyCardScript6;
-    public EntropyCardScript entropyCardScript7;
-    public EntropyCardScript entropyCardScript8;
-    public EntropyCardScript entropyCardScript9;
-    public EntropyCardScript entropyCardScript10;
-    public EntropyCardScript entropyCardScript11;
+    //Bag Of Tricks
+    public EntropyCardScript entropyCardBagScript1;
+    public EntropyCardScript entropyCardBagScript2;
+    public EntropyCardScript entropyCardBagScript3;
+    public EntropyCardScript entropyCardBagScript4;
+    public EntropyCardScript entropyCardBagScript5;
+    public EntropyCardScript entropyCardBagScript6;
+    public EntropyCardScript entropyCardBagScript7;
+    public EntropyCardScript entropyCardBagScript8;
+    public EntropyCardScript entropyCardBagScript9;
+    public EntropyCardScript entropyCardBagScript10;
+    public EntropyCardScript entropyCardBagScript11;
+    public EntropyCardScript entropyCardBagScript12;
+    public EntropyCardScript entropyCardBagScript13;
+    public EntropyCardScript entropyCardBagScript14;
+    public EntropyCardScript entropyCardBagScript15;
+    public EntropyCardScript entropyCardBagScript16;
+    public EntropyCardScript entropyCardBagScript17;
+    public EntropyCardScript entropyCardBagScript18;
+    public EntropyCardScript entropyCardBagScript19;
+    //Lighting
+    public EntropyCardScript entropyCardLightingScript1;
+    public EntropyCardScript entropyCardLightingScript2;
+    public EntropyCardScript entropyCardLightingScript3;
+    public EntropyCardScript entropyCardLightingScript4;
+    public EntropyCardScript entropyCardLightingScript5;
+    public EntropyCardScript entropyCardLightingScript6;
+    public EntropyCardScript entropyCardLightingScript7;
+    //Extensive Experience
+    public EntropyCardScript entropyCardExtensiveScript1;
+    public EntropyCardScript entropyCardExtensiveScript2;
+    public EntropyCardScript entropyCardExtensiveScript3;
+    public EntropyCardScript entropyCardExtensiveScript4;
+    public EntropyCardScript entropyCardExtensiveScript5;
+    //shared fate
+    public EntropyCardScript entropyChardSharedScript1;
+    public EntropyCardScript entropyChardSharedScript2;
+    
 
     List<EntropyCardScript> entropyCardDeck = new List<EntropyCardScript>();
     public List<EntropyCardScript> getentropyCards()
     {
-        entropyCardDeck.Add(entropyCardScript1);
-        entropyCardDeck.Add(entropyCardScript2);
-        entropyCardDeck.Add(entropyCardScript3);
-        entropyCardDeck.Add(entropyCardScript4);
-        entropyCardDeck.Add(entropyCardScript5);
-        entropyCardDeck.Add(entropyCardScript6);
-        entropyCardDeck.Add(entropyCardScript7);
-        entropyCardDeck.Add(entropyCardScript8);
-        entropyCardDeck.Add(entropyCardScript9);
-        entropyCardDeck.Add(entropyCardScript10);
-        entropyCardDeck.Add(entropyCardScript11);
+        entropyCardDeck.Add(entropyCardBagScript1);
+        entropyCardDeck.Add(entropyCardBagScript2);
+        entropyCardDeck.Add(entropyCardBagScript3);
+        entropyCardDeck.Add(entropyCardBagScript4);
+        entropyCardDeck.Add(entropyCardBagScript5);
+        entropyCardDeck.Add(entropyCardBagScript6);
+        entropyCardDeck.Add(entropyCardBagScript7);
+        entropyCardDeck.Add(entropyCardBagScript8);
+        entropyCardDeck.Add(entropyCardBagScript9);
+        entropyCardDeck.Add(entropyCardBagScript10);
+        entropyCardDeck.Add(entropyCardBagScript11);
+        entropyCardDeck.Add(entropyCardBagScript12);
+        entropyCardDeck.Add(entropyCardBagScript13);
+        entropyCardDeck.Add(entropyCardBagScript14);
+        entropyCardDeck.Add(entropyCardBagScript15);
+        entropyCardDeck.Add(entropyCardBagScript16);
+        entropyCardDeck.Add(entropyCardBagScript17);
+        entropyCardDeck.Add(entropyCardBagScript18);
+        entropyCardDeck.Add(entropyCardBagScript19);
+
+        entropyCardDeck.Add(entropyCardLightingScript1);
+        entropyCardDeck.Add(entropyCardLightingScript2);
+        entropyCardDeck.Add(entropyCardLightingScript3);
+        entropyCardDeck.Add(entropyCardLightingScript4);
+        entropyCardDeck.Add(entropyCardLightingScript5);
+        entropyCardDeck.Add(entropyCardLightingScript6);
+        entropyCardDeck.Add(entropyCardLightingScript7);
+        
+        entropyCardDeck.Add(entropyCardExtensiveScript1);
+        entropyCardDeck.Add(entropyCardExtensiveScript2);
+        entropyCardDeck.Add(entropyCardExtensiveScript3);
+        entropyCardDeck.Add(entropyCardExtensiveScript4);
+        entropyCardDeck.Add(entropyCardExtensiveScript5);
+
+        entropyCardDeck.Add(entropyChardSharedScript1);
+        entropyCardDeck.Add(entropyChardSharedScript2);
+
         return entropyCardDeck;
     }
 }

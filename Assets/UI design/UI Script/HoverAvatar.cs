cdec63522e53b4a6ae95b4b17ad31cc2c4eb954a
GohXinYee <sfyxg1@nottingham.edu.my>
2021-01-13 03:38:30 +0000

    UI avatar profile

---
+++ Assets/UI design/UI Script/HoverAvatar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverAvatar : MonoBehaviour
+{
+    public GameObject avatarprofile;
+    Vector3 unhoverscale;
+
+    public void OnHoverEnter()
+    {
+        unhoverscale = avatarprofile.transform.localScale;
+        avatarprofile.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+    }
+
+    public void OnHoverExit()
+    {
+      
+        avatarprofile.transform.localScale = unhoverscale;
+    }
+}

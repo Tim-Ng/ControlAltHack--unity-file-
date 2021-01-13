8e15a5538ec24863416df17ae731e9e118966a65
Ng Yao Hong <efyyn2@nottingham.edu.my>
2021-01-13 08:03:23 +0000

    Edited HoverAvatar so that it would acount if we are detecting if the object has image and act accordingl The event avertar is also used when your selecting your avertar Futher more doing prefeb for avertar

--- Assets/UI design/UI Script/HoverAvatar.cs
+++ Assets/UI design/UI Script/HoverAvatar.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HoverAvatar : MonoBehaviour
 {
-    public GameObject avatarprofile;
+    [SerializeField]private GameObject avatarprofile = null;
+    [SerializeField] private bool detectsHaveSprite;
     Vector3 unhoverscale;
 
     public void OnHoverEnter()
     {
         unhoverscale = avatarprofile.transform.localScale;
-        avatarprofile.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        if (detectsHaveSprite)
+        {
+            if(avatarprofile.GetComponent<Image>().sprite != null)
+            {
+                avatarprofile.transform.localScale = new Vector3(1.2f, 1.2f);
+            }
+        }
+        else
+        {
+            avatarprofile.transform.localScale = new Vector3(1.2f, 1.2f);
+        }
+            
     }
 
     public void OnHoverExit()
